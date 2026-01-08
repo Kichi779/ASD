ebc1e7e3db070d2d8e3e4096ee6b899dc99b2ce1
Kichi779 <kichi779@protonmail.com>
2026-01-08 23:42:23 +0300

    Test

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Eklentilerin otomatik kaydı için bu satır şart
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
